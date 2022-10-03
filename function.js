6aa9d6f9603724b8ecba158f5923c53d0b02d1d4
sumona <63995737+Meherjavienothoy@users.noreply.github.com>
2022-10-03 11:44:26 +1100

    first commit

---
+++ function.js
@@ -0,0 +1,20 @@
+/* 
+function function Name Likhte hbe nijer moto kre(){
+    console.log('hello everyone')
+}
+function Er name likhte hbe();
+*/
+
+//Example-1
+function sayLoveYou(){
+    console.log('hello everyone')
+}
+sayLoveYou();
+
+
+
+//Example-2
+function sumona(){
+    console.log('My name is othoy')
+}
+sumona();
