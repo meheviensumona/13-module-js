326449a1b2d9ee08b24c36c61529febad70bf39a
sumona <63995737+Meherjavienothoy@users.noreply.github.com>
2022-10-03 11:04:44 +1100

    2nd commit

---
+++ switch.js
@@ -0,0 +1,18 @@
+ // switch
+ var num = 3
+ switch(num){
+    case 1000:
+        console.log('I am 1000')
+        break;
+    case 100:
+        console.log('I am 100')
+        break;
+    case 10:
+        console.log('I an 10')
+        break;
+    case 4:
+        console.log('I am 4')
+        break;
+        default:
+            console.log('I don t know  who you are')
+ }
